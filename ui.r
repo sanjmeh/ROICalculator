03eec96dcd5dd190eb2fcc152b8ea9c2f8d9f8b0
Inesh Tandon <i08polymath@gmail.com>
2024-06-03 13:22:07 +0530

    Updated: Server.R/ manpower section has been refactored and fucntionality for hiding and yes/no questions added using uiOutput and renderUI Ui.R/ manpower section refactoring

--- ui.r
+++ ui.r
@@ -1,10 +1,13 @@
 library(shiny)
+library(shinyBS)
 library(bslib)
 library(shinydashboard)
 library(DT)
 library(plotly)
 library(ggplot2)
 library(shinyalert)
+
+
 
 ui <- shinyUI(fluidPage(
 
@@ -26,105 +29,190 @@ ui <- shinyUI(fluidPage(
 
   h2("Universal Values"),
   fluidRow(column(width=2,
-                  numericInput("shift_count","Number of shifts",value = 2)
-  ),
-  column(width=2,
-         numericInput("hemm_count","Number of HEMM",value=100)
-  ),
-  column(width=2,
-         numericInput("truck_count","Number of diesel bowsers/site",value = 7,min=0,max=10),
-  ),
-  column(width=2,
-         numericInput("hemm_daily_consump","HEMM Fuel Consumption/Day",value=100)
-  ),
-  column(width=2,
-         numericInput("sfs_count","Number of Fuel Stations",value=2),
-  ),
-  column(width=2,
-         numericInput("fuel_entry_count", "Number of refuels-entries/all Hemm/day:",value = 200),
-  )
+                  numericInput("shift_count","Number of shifts",value = 2)),
+           column(width=2,
+                  numericInput("hemm_count","Number of HEMM",value=100)),
+           column(width=2,
+                  numericInput("truck_count","Number of diesel bowsers/site",value = 3,min=0,max=10)),
+           column(width=2,
+                  numericInput("hemm_daily_consump","HEMM Fuel Consumption/Day",value=100)),
+           column(width=2,
+                  numericInput("sfs_count","Number of Fuel Stations",value=2)),
+           column(width=2,
+                  uiOutput("fuel_entry_count_check"))
   ),
 
 
   navbarPage("Menu",
 
-             # MANPOWER TAB
-
-             tabPanel("Manpower Calculation",
-                      h1("Enter Work Parameters"),
+             tabPanel("Manpower",
                       sidebarLayout(
                         sidebarPanel(
                           fluidRow(
-                            column(6,numericInput("logger_count_per_bowser","Fuel Logger/Bowser",value=1)),
-                            column(6,numericInput("manpower_save_fdl","% Saving in Fuel Logger",value=86))),
-                          div(
-                            h5("Total Fuel Loggers:"),
-                            verbatimTextOutput("logger_count",TRUE)),
-
-                          fluidRow(column(6,h5("Entries per year: (entries/day * 365)"),verbatimTextOutput("entries_per_year")),
-                                   column(6,h5("Data Entry Operators employed annually."),verbatimTextOutput("data_entry_count"))),
-
-                          # h6("Assuming that 5% of the entries made by the operator will be erroneous and hence will require correction."),
-                          # h6("5 mins per entry ~ 3 mins for entry and 2 mins for correction"),
-                          actionButton("dto_count_info", "Info",
-                                       icon("lightbulb"),
-                                       style="color: #fff; background-color: #008000; border-color: #2e6da4"),
-                          fluidRow(column(6,numericInput("error_margin","Enter % of erroneous entries",value=5)),
-                                   column(6,
-                                          numericInput("manpower_save_dto","% Saving in Data Entry Operators",value=75))),
-                          br(),
-                          fluidRow(column(6,
-                                          numericInput("coordinator_count"," Fuel Dispatchers/Shift",value=2)),
-                                   column(6,
-                                          numericInput("manpower_save_fdc","% Saving in Fuel Dispatcher",value=50))),
-                          fluidRow(column(6,
-                                          numericInput("accountant_count","Accountants required: ",value=3)),
-                                   column(6,
-                                          numericInput("manpower_save_accounts","% Saving in Accountants",value=66))),
-
-
-                          br(),
-                          sliderInput("fuel_logger_cost","Avg cost of fuel logger: ",value=150000,min=100000,max=300000),
-                          sliderInput("fuel_dispatcher_cost","Avg cost of fuel dispatch coordinator: ",value=500000,min=100000,max=1000000),
-                          sliderInput("data_entry_emp","Avg cost of data entry FTE: ",value=300000,min=100000,max=500000),
-                          sliderInput("accountant_cost","Avg cost of accountant FTE: ",value=300000,min=100000,max=800000),
-                        ),
-                        mainPanel(
-                          fluidPage(
-                            column(8,
-                                   fluidRow(
-                                     splitLayout(
-                                       tableOutput("manpower_data"),
-                                       tableOutput("manpower_data_2")
+                            column(12,
+                                   div(
+                                     fluidRow(column(9,h3("Step 1: Enter FTE Details")),column(3,br(),actionButton("manpower_info_button", "Info", icon = icon("info-circle")))),
+                                     radioButtons("manpower_dispatch_q","Do you have Fuel Dispatchers for scheduling Fuel Bowser Trips?",
+                                                  choices = c("Yes" = TRUE, "No" = FALSE),
+                                                  inline = TRUE),
+                                     numericInput("coordinator_count"," Fuel Dispatchers/Shift",value=2),
+                                     radioButtons("manpower_logger_q","Do you have Fuel Data Loggers for logging on-site fuel transactions?",
+                                                  choices = c("Yes" = TRUE, "No" = FALSE),
+                                                  inline = TRUE),
+                                     uiOutput("manpower_logger_check"),
+                                     radioButtons("manpower_dte_q","Do you have Data Entry Operators for manual backend updation?",
+                                                  choices = c("Yes" = TRUE, "No" = FALSE),
+                                                  inline = TRUE,
+                                                  selected = TRUE),
+                                     uiOutput("error_margin_check"),
+                                     uiOutput("manpower_acc_check")
                                      )
-                                   ),
-                                   fluidRow(
-                                     plotlyOutput("histogram"),
-                                   ),
-                                   fluidRow(
-                                     plotOutput("pieChart")
-                                   ),
+                                   )
                             ),
-                            column(4,
-                                   h5("Current Cost of Manpower (₹):"),
-                                   verbatimTextOutput("manpower_summation_current"),
-                                   h5("Revised Cost of Manpower (₹):"),
-                                   verbatimTextOutput("manpower_summation"),
-                                   h5("Employed FTE count:"),
-                                   verbatimTextOutput("manpower_fte_total"),
-                                   h3("Assumptions"),
-                                   p("For a data entry full time employee(FTE): woking 8 hour shift, 5 hours of productivity is considered in calculation of data entry operators."),
-                                   br(),
-                                   br(),
-                                   p("Data Aggregator/Compiler FTE: is estimated at 5 LPA * number of compilers required to meet requirements in a year."),
-                                   br(),
-                                   br(),
-                                   p("Cost of Correction: Calculating from the provided margin of errors, number of additional working hours are estimated and number of data entry operators required are calculated")
-                            ),
+                          br(),
+                          fluidRow(
+                            column(12,
+                                   div(
+                                     fluidRow(column(9,h3("CTC Input:")),column(3,br(),actionButton("manpower_ctc_info_button", "Info", icon = icon("info-circle")))),
+
+                                     sliderInput("fuel_dispatcher_cost","Avg Annual CTC of Fuel Dispatcher: ",value=500000,min=100000,max=1000000),
+                                     sliderInput("fuel_logger_cost","Avg Annual CTC of Fuel Logger: ",,value=150000,min=100000,max=300000),
+                                     sliderInput("data_entry_cost","Avg Annual CTC of Data Entry Operator FTE: ",value=300000,min=100000,max=500000),
+                                     sliderInput("accountant_cost","Avg Annual CTC of Accountant FTE: ",value=500000,min=100000,max=800000),
+                                     )
+                                   )
+                            )
+                          ),
+                        mainPanel(fluidPage(
+                          column(8,
+                                 fluidRow(
+                                   tableOutput("manpower_data")
+                                 ),
+                                 fluidRow(
+                                   plotlyOutput("histogram"),
+                                 )
+                          ),
+                          column(4,
+                                 h5("Current Cost of Manpower (₹):"),
+                                 verbatimTextOutput("manpower_summation_current"),
+                                 h5("Revised Cost of Manpower (₹):"),
+                                 verbatimTextOutput("manpower_summation"),
+                                 h5("Employed FTE count:"),
+                                 verbatimTextOutput("manpower_fte_total"),
+                                 h3("How you can acheive savings with MindShift:"),
+                                 p("MindShift offers the capability to automate manual data entry, updates, and analysis processes, facilitating a transition to a time-efficient fuel management method.\n
+                                   This transition leads to cost savings and increased productivity for your organization.")),
                           )
-                        )
-                      )
-             ),
+                          )
+                        ),
+                      bsModal("manpower_info_modal", "Manpower Roles Information:", "manpower_info_button", size = "large",
+                              p("This section provides a concise overview of the duties associated with various personnel positions."),
+                              h4("More Details:"),
+                              tags$ul(
+                                tags$li(
+                                  h3("Fuel Dispatchers:"),
+                                  p("A fuel dispatcher oversees and coordinates the scheduling of diesel bowser trips to Heavy Earth Moving Machinery (HEMM) in each operational shift, ensuring timely refueling services are provided as required."),
+                                ),
+                                tags$li(
+                                  h3("Fuel Loggers:"),
+                                  p("These individuals manually log HMR and fuel transaction from a bowser to an HEMM on paper"),
+                                ),
+                                tags$li(
+                                  h3("Data Entry Operators:"),
+                                  p("A data entry operator is tasked with the responsibility of transcribing manually recorded logs and inputting the data into the backend system."),
+                                  p("The allocation of data entry operators is determined by the volume of entries recorded per day, facilitating efficient management of yearly entries and the corresponding upload timeframe.")
+                                ),
+                                tags$li(
+                                  h3("Accountants:"),
+                                  p("Accountants are tasked with the responsibility of verifying the accuracy of data entered into the backend system, rectifying any discrepancies as needed."),
+                                  p("They monitor the operational hours of Heavy Earth Moving Machinery (HEMM) and reconcile fuel consumption data with fuel dispensing records.")
+                                ),
+                                )
+                              ),
+                      bsModal("manpower_ctc_info_modal", "Manpower Roles Information:", "manpower_ctc_info_button", size = "large",
+                              p("In an attempt to estimate Return of Investment, we are trying to acquire cost incurred for a single FTE across different roles.")
+                              )
+                      ),
+
+
+
+             # MANPOWER TAB ORIGINAL
+
+             # tabPanel("Manpower Calculation",
+             #          h1("Enter Work Parameters"),
+             #          sidebarLayout(
+             #            sidebarPanel(
+             #              fluidRow(
+             #                column(6,numericInput("logger_count_per_bowser","Fuel Logger/Bowser",value=1)),
+             #                column(6,numericInput("manpower_save_fdl","% Saving in Fuel Logger",value=86))),
+             #              div(
+             #                h5("Total Fuel Loggers:"),
+             #                verbatimTextOutput("logger_count",TRUE)),
+             #
+             #              fluidRow(column(6,h5("Entries per year: (entries/day * 365)"),verbatimTextOutput("entries_per_year")),
+             #                       column(6,h5("Data Entry Operators employed annually."),verbatimTextOutput("data_entry_count"))),
+             #
+             #              # h6("Assuming that 5% of the entries made by the operator will be erroneous and hence will require correction."),
+             #              # h6("5 mins per entry ~ 3 mins for entry and 2 mins for correction"),
+             #              actionButton("dto_count_info", "Info",
+             #                           icon("lightbulb"),
+             #                           style="color: #fff; background-color: #008000; border-color: #2e6da4"),
+             #              fluidRow(column(6,numericInput("error_margin","Enter % of erroneous entries",value=5)),
+             #                       column(6,
+             #                              numericInput("manpower_save_dto","% Saving in Data Entry Operators",value=75))),
+             #              br(),
+             #              fluidRow(column(6,
+             #                              numericInput("coordinator_count"," Fuel Dispatchers/Shift",value=2)),
+             #                       column(6,
+             #                              numericInput("manpower_save_fdc","% Saving in Fuel Dispatcher",value=50))),
+             #              fluidRow(column(6,
+             #                              numericInput("accountant_count","Accountants required: ",value=3)),
+             #                       column(6,
+             #                              numericInput("manpower_save_accounts","% Saving in Accountants",value=66))),
+             #
+             #
+             #              br(),
+             # sliderInput("fuel_logger_cost","Avg cost of fuel logger: ",value=150000,min=100000,max=300000),
+             # sliderInput("fuel_dispatcher_cost","Avg cost of fuel dispatch coordinator: ",value=500000,min=100000,max=1000000),
+             # sliderInput("data_entry_cost","Avg cost of data entry FTE: ",value=300000,min=100000,max=500000),
+             # sliderInput("accountant_cost","Avg cost of accountant FTE: ",value=300000,min=100000,max=800000),
+             #            ),
+             #            mainPanel(
+             #              fluidPage(
+             #                column(8,
+             #                       fluidRow(
+             #                         splitLayout(
+             #                           tableOutput("manpower_data"),
+             #                           tableOutput("manpower_data_2")
+             #                         )
+             #                       ),
+             #                       fluidRow(
+             #                         # plotlyOutput("histogram"),
+             #                       ),
+             #                       fluidRow(
+             #                         plotOutput("pieChart")
+             #                       ),
+             #                ),
+             #                column(4,
+             #                       h5("Current Cost of Manpower (₹):"),
+             #                       verbatimTextOutput("manpower_summation_current"),
+             #                       h5("Revised Cost of Manpower (₹):"),
+             #                       verbatimTextOutput("manpower_summation"),
+             #                       h5("Employed FTE count:"),
+             #                       verbatimTextOutput("manpower_fte_total"),
+             #                       h3("Assumptions"),
+             #                       p("For a data entry full time employee(FTE): woking 8 hour shift, 5 hours of productivity is considered in calculation of data entry operators."),
+             #                       br(),
+             #                       br(),
+             #                       p("Data Aggregator/Compiler FTE: is estimated at 5 LPA * number of compilers required to meet requirements in a year."),
+             #                       br(),
+             #                       br(),
+             #                       p("Cost of Correction: Calculating from the provided margin of errors, number of additional working hours are estimated and number of data entry operators required are calculated")
+             #                ),
+             #              )
+             #            )
+             #          )
+             # ),
 
              # PILFERAGE
 
@@ -320,7 +408,11 @@ ui <- shinyUI(fluidPage(
                         fluidRow(column(4,
                                         fluidRow(
                                           h3("Manpower Section"),
-                                          numericInput("correction_time","Time taken for erroneous entry correction",value=10)
+                                          numericInput("correction_time","Time taken for erroneous entry correction",value=10),
+                                          numericInput("manpower_reduction_dispatcher","Predicted Reduction in Fuel Dispatchers:",value=1),
+                                          numericInput("manpower_reduction_logger","Predicted Reduction in Fuel Data Loggers:",value=4),
+                                          numericInput("manpower_reduction_dte","Predicted Reduction in Data Entry Operators:",value=2),
+                                          numericInput("manpower_reduction_accountant","Predicted Reduction in Accountants:",value=1)
                                         )
                         ),
                         column(4,
